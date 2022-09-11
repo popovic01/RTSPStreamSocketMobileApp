02771e9691ad8506d901c7899a80fce3dfde178b
popovic01 <mimica2001@gmail.com>
2022-09-11 10:53:09 +0200

    Verzija 5 - internet checking

--- app/src/main/java/com/example/firebot/views/MainActivity.kt
+++ app/src/main/java/com/example/firebot/views/MainActivity.kt
@@ -2,18 +2,27 @@ package com.example.firebot.views
 
 import android.content.DialogInterface
 import android.graphics.PixelFormat
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.*
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.firebot.R
-import org.videolan.libvlc.LibVLC;
-import org.videolan.libvlc.Media;
-import org.videolan.libvlc.MediaPlayer;
-import org.videolan.libvlc.util.VLCVideoLayout;
+import com.github.pwittchen.reactivenetwork.library.rx2.Connectivity
+import com.github.pwittchen.reactivenetwork.library.rx2.ConnectivityPredicate
+import com.github.pwittchen.reactivenetwork.library.rx2.ReactiveNetwork
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import org.videolan.libvlc.LibVLC
+import org.videolan.libvlc.Media
+import org.videolan.libvlc.MediaPlayer
+import org.videolan.libvlc.util.VLCVideoLayout
+
 
 class MainActivity : AppCompatActivity(), JoystickView.JoystickListener {
 
@@ -24,6 +33,7 @@ class MainActivity : AppCompatActivity(), JoystickView.JoystickListener {
 
     val negativeButtonClick = { dialog: DialogInterface, which: Int -> }
     var currentCamera = "RGB"
+    lateinit var tvInternet: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,15 +48,28 @@ class MainActivity : AppCompatActivity(), JoystickView.JoystickListener {
         window.setBackgroundDrawableResource(R.color.black)
         setContentView(R.layout.activity_main)
 
+        tvInternet = findViewById<TextView>(R.id.tvInternet)
+
         val joystickView = findViewById<JoystickView>(R.id.joystick)
         //code to make the view transparent
         joystickView.setZOrderOnTop(true)
         val joystickViewHolder = joystickView.holder
         joystickViewHolder.setFormat(PixelFormat.TRANSPARENT)
 
-        libVlc = LibVLC(this);
-        _mediaPlayer = MediaPlayer(libVlc)
-        videoLayout = findViewById(R.id.videoLayout);
+        //observing network state
+        ReactiveNetwork
+            .observeNetworkConnectivity(applicationContext) //it is better to pass applicationContext
+            .subscribeOn(Schedulers.io())
+            //.filter(ConnectivityPredicate.hasType(ConnectivityManager.TYPE_WIFI))
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe { connectivity: Connectivity? ->
+                if (connectivity?.state() == NetworkInfo.State.CONNECTED)
+                    startVideo() //if device is connected to wifi, start the stream
+                else
+                    stopVideo()
+                Log.d("Main aktivnost", "${connectivity?.state()}")
+            }
+
     }
 
     fun withItems(view: View) {
@@ -69,18 +92,31 @@ class MainActivity : AppCompatActivity(), JoystickView.JoystickListener {
         }
     }
 
-    override fun onStart() {
-        super.onStart()
+    fun startVideo() {
+        tvInternet.visibility = View.INVISIBLE
+        libVlc = LibVLC(this);
+        _mediaPlayer = MediaPlayer(libVlc)
+        videoLayout = findViewById(R.id.videoLayout);
 
         _mediaPlayer!!.attachViews(videoLayout!!, null, false, false)
 
-        val media = Media(libVlc, Uri.parse("rtsp://192.168.10.122:8554/mystream"))
+        val media = Media(libVlc, Uri.parse("rtsp://192.168.119.158:8554/mystream"))
         media.setHWDecoderEnabled(true, false)
         media.addOption(":network-caching=600")
 
         _mediaPlayer!!.setMedia(media)
         media.release()
         _mediaPlayer!!.play()
+    }
+
+    fun stopVideo() {
+        tvInternet.visibility = View.VISIBLE
+        _mediaPlayer!!.stop()
+        _mediaPlayer!!.detachViews()
+        _mediaPlayer!!.release()
+        libVlc!!.release()
+        libVlc = null
+        _mediaPlayer = null
     }
 
     override fun onJoystickMoved(xPercent: Float, yPercent: Float, id: Int) {
